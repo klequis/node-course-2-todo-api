d2447762127d6e3636e9671e0f4a5cab8ebff8fd
klequis <klequis@gmail.com>
2018-04-13 19:54:50 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
+var chalk = require('chalk')
 
 var {
   mongoose
@@ -32,7 +34,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  });
+  })
+})
+
+//GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  console.log('id', id) // 5ad134a43b18c9382be747e9
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('<p style="color: red">Id is not valid</p>')
+  } else {
+    console.log(chalk.green('Id is valid'));
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('cound not find todo')
+    }
+    res.send({todo})
+  }, (e) => {
+    res.status(400).send() // don't send the error, it may have sensitive information
+  })
 })
 
 app.listen(4000, () => {
