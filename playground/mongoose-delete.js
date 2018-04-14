43901ba1c5d289892267849334cfbbf6e437d7bf
klequis <klequis@gmail.com>
2018-04-14 13:35:57 -0700

    Test the DELETE /todos/:id route

--- playground/mongoose-delete.js
+++ playground/mongoose-delete.js
@@ -7,3 +7,7 @@ const {user} = require('./../server/models/user')
 Todo.findByIdAndRemove('5ad239894501c05fcf827df1').then((result) => {
   console.log(result);
 });
+
+
+Todo.findOneAndRemove({_id: '57c4670dbb35fcbf6fda1154'}).then((todo) => {
+});
