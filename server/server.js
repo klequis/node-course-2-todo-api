d5fd5d435eb29197054fd6d3dbd94bda1d64f8b1
klequis <klequis@gmail.com>
2018-04-14 08:44:22 -0700

    fix to match book source

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
+var chalk = require('chalk')
+
+
 var express = require('express')
 var bodyParser = require('body-parser')
-const { ObjectID } = require('mongodb')
-var chalk = require('chalk')
+var {
+  ObjectID
+} = require('mongodb')
 
 var {
   mongoose
 } = require('./db/mongoose')
-
 var {
   Todo
 } = require('./models/todo')
-
 var {
   User
 } = require('./models/user')
-
-// ================
 
 var app = express()
 const port = process.env.PORT || 4000
@@ -24,6 +24,7 @@ app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({text: req.body.text})
+
   todo.save().then((doc) => {
     res.send(doc)
   }, (e) => {
@@ -33,28 +34,72 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({todos})
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send(e)
   })
 })
 
 //GET /todos/12345
 app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  console.log('id', id) // 5ad134a43b18c9382be747e9
+  var id = req.params.id
+
   if (!ObjectID.isValid(id)) {
-    return res.status(404).send('<p style="color: red">Id is not valid</p>')
-  } else {
-    console.log(chalk.green('Id is valid'));
+    return res.status(404).send()
   }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(404).send('cound not find todo')
+      return res.status(404).send()
     }
     res.send({todo})
-  }, (e) => {
-    res.status(400).send() // don't send the error, it may have sensitive information
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
   })
 })
 
